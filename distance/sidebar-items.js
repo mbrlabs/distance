ea9bca7d348105eb085fe4dc9429348882bfd5ec
Marcus Brummer <mbrlabs7@gmail.com>
2016-06-28 01:13:57 +0200

    Added documentation

---
+++ distance/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["levenshtein","Levenshtein distance"]]});
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["distance"] = {"doc":"# A collection of approximate string matching algorithms","items":[[5,"levenshtein","distance","# Levenshtein distance",null,{"inputs":[{"name":"str"},{"name":"str"}],"output":{"name":"i32"}}]],"paths":[]};
+initSearch(searchIndex);
